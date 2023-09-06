fe25d5d21f69f65a6cca0c4aa0d69ee70014b91d
Morris <wesley.g.morris@vanderbilt.edu>
2023-09-06 11:12:10 -0500

    adding some things to test the file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,19 @@
 import streamlit as st
+import pandas as pd
 import numpy as np
+from transformers import pipeline
 
-st.markdown('## Iris Species Prediction')
+pipe = pipeline('text-classification', model='tiedaar/short-answer-classification')
+df = pd.read_csv('../data/subsections.csv')
+sample_row = df.sample(n=1)
+
+passage = sample_row['clean_text']
+question = sample_row['question']
+
+st.markdown('## Short answer scoring demonstration')
+st.markdown('### Passage')
+st.markdown(passage)
+
 sepal_length = st.number_input('sepal length (cm)')
 sepal_width = st.number_input('sepal width (cm)')
 petal_length = st.number_input('petal length (cm)')
