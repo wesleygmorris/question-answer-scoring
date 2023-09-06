8a4f8dd6a4ce7b339173ee3fac4ba439e929e607
Morris <wesley.g.morris@vanderbilt.edu>
2023-09-06 11:00:02 -0500

    adding some things to test the file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
-print('hello world')
+import streamlit as st
+import numpy as np
+
+st.markdown('## Iris Species Prediction')
+sepal_length = st.number_input('sepal length (cm)')
+sepal_width = st.number_input('sepal width (cm)')
+petal_length = st.number_input('petal length (cm)')
+petal_width = st.number_input('petal width (cm)')
